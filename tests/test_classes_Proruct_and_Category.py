cb05e68f56e4f532d9ff151af0bbc39c9daf98ea
Alena <alittlekindness94@gmail.com>
2024-11-13 12:35:32 +0500

    обновлены тесты

--- tests/test_classes_Proruct_and_Category.py
+++ tests/test_classes_Proruct_and_Category.py
@@ -1,5 +1,5 @@
-from src.classes import Product, Category
 import pytest
+from src.classes import Product, Category
 
 
 class TestProduct:
@@ -15,8 +15,8 @@ class TestProduct:
         product.price = 50.0
         assert product.price == 50.0
 
-        product.price = -10  # Это должно вызвать сообщение
-        assert product.price == 50.0  # Цена не должна измениться
+        product.price = -10
+        assert product.price == 50.0
 
     def test_new_product_class_method(self):
         product_dict = {
@@ -37,15 +37,15 @@ class TestCategory:
         category = Category(name="Фрукты", description="Свежие фрукты")
         assert category.name == "Фрукты"
         assert category.description == "Свежие фрукты"
-        assert category.products == ""  # Пустая категория не должна иметь продуктов
+        assert category.products == ""
 
     def test_add_product(self):
         category = Category(name="Фрукты", description="Свежие фрукты")
         product = Product(name="Яблоко", description="Свежие красные яблоки", price=30.0, quantity=100)
         category.add_product(product)
 
-        assert len(category._Category__products) == 1  # Проверяем, что продукт добавлен
-        assert category.products == "Яблоко, 30.0 руб. Остаток: 100 шт."  # Проверяем вывод
+        assert len(category._Category__products) == 1
+        assert category.products == "Яблоко, 30.0 руб. Остаток: 100 шт."
 
     def test_multiple_products(self):
         category = Category(name="Фрукты", description="Свежие фрукты")
@@ -55,12 +55,12 @@ class TestCategory:
         category.add_product(product1)
         category.add_product(product2)
 
-        assert len(category._Category__products) == 2  # Проверяем, что два продукта добавлены
+        assert len(category._Category__products) == 2
         expected_output = (
             "Яблоко, 30.0 руб. Остаток: 100 шт.\n"
             "Груша, 25.0 руб. Остаток: 80 шт."
         )
-        assert category.products == expected_output  # Проверяем вывод
+        assert category.products == expected_output
 
 
 if __name__ == "__main__":
